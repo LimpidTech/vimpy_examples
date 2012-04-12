ae892793128bcd9dd650e9c837a1444277655f0f
Brandon R. Stoner <monokrome@LimpidTech.com>
2012-04-12 00:39:03 -0600

    Modified Vimpy plugin to use settings.

--- vimpyexample/plugin.py
+++ vimpyexample/plugin.py
@@ -1,9 +1,14 @@
 from vimpy.plugins import Plugin
+from vimpy.conf import settings
+
+settings['g:vimpy_example_left_message'] = 'Vim left.'
 
 class VimpyExamplePlugin(Plugin):
     def vim_enter(self, data=None):
         print('VimpyExample :: Vim entered.')
 
     def vim_leave(self, data=None):
-        print('VimpyExample :: Vim left.')
+        left_message = settings['g:vimpy_example_left_message']
+
+        print('VimpyExample :: {message}'.format(message=left_message))
 
