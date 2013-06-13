bd930bd776abbe0d8bff78601badfff389223c9c
Brandon R. Stoner <monokrome@limpidtech.com>
2013-06-13 01:01:53 -0700

    Updated plugin for the newer, smarter Vimpy.

--- vimpyexample/__init__.py
+++
@@ -1,5 +0,0 @@
-from .plugin import VimpyExamplePlugin
-
-# Instantiate a plugin. This step will be removed by the release of Vimpy 0.2
-VimpyExamplePlugin()
-
--- vimpyexample/plugin.py
+++ vimpyexample/plugin.py
@@ -1,7 +1,7 @@
 from vimpy.plugins import Plugin
-from vimpy.conf import settings
+from vimpy import variables
 
-settings['g:vimpy_example_left_message'] = 'Vim left.'
+variables.globals['vimpy_example_left_message'] = 'Vim left.'
 
 class VimpyExamplePlugin(Plugin):
     def vim_enter(self, data=None):
@@ -10,6 +10,6 @@ class VimpyExamplePlugin(Plugin):
     def vim_leave(self, data=None):
         # Try changing this in the vim terminal before quitting.
         # :let g:vimpy_example_left_message="Goodbye."
-        left_message = settings['g:vimpy_example_left_message']
+        left_message = variables.globals['vimpy_example_left_message']
 
         print('VimpyExample :: {message}'.format(message=left_message))
