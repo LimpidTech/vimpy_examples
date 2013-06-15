02a15815d25576c7af2929b2d04470de6144a0b0
Brandon R. Stoner <monokrome@limpidtech.com>
2013-06-14 23:44:56 -0700

    Docs!

--- commands.py
+++ commands.py
@@ -2,7 +2,19 @@ from vimpy import Command, variables
 
 
 class VimpyExample(Command):
+    """ A command for changing variables related vimpy_example's Plugin. """
+
     def run(self, message=None):
+        """ This function gets called with :VimpyExample.
+
+        This function gets called when executing the :VimpyExample command
+        from Vim. It can be used in the following ways:
+
+        :VimpyExample
+        :VimpyExample "A new message."
+        :VimpyExample message=hi
+
+        """
 
         if message is None:
             if 'vimpy_example_left_message' in variables.globals:
